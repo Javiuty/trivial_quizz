a0c9c4fddc9770cf1440d7dd29b29989f4bb4629
Javiuty <javiiescuadra@gmail.com>
2020-06-23 20:32:10 +0200

    logica geografia

--- js/geography.js
+++ js/geography.js
@@ -4,7 +4,21 @@ fetch("https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=
     })
     .then(function(data){
         let container = document.getElementById("geografia__contenedor-main")
-        container.innerHTML = data.results[0].question
-        
+        let phase = 0;
+        container.innerHTML = data.results[phase].question
+
+        console.log(data)
+
+        let respuesta1 = document.getElementById("respuesta-1")
+        let respuesta2 = document.getElementById("respuesta-2")
+        let respuesta3 = document.getElementById("respuesta-3")
+        let respuesta4 = document.getElementById("respuesta-4")
+
+        respuesta1.innerHTML = data.results[phase].incorrect_answers[0]
+        respuesta2.innerHTML = data.results[phase].correct_answer
+        respuesta3.innerHTML = data.results[phase].incorrect_answers[1]
+        respuesta4.innerHTML = data.results[phase].incorrect_answers[2]
+
+                
     });
 
