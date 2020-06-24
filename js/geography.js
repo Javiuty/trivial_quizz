e9ca4ffa56218092103e4601856040436f2a986b
Javiuty <javiiescuadra@gmail.com>
2020-06-24 14:50:19 +0200

    mas logica compte geografia

--- js/geography.js
+++ js/geography.js
@@ -11,16 +11,71 @@ fetch(
 
 //Función donde guardamos los datos obtenidos del fetch
 function showData(data) {
+  console.log(data);
   let container = document.getElementById("geografia__contenedor-main");
   let phase = 0;
   container.innerHTML = data.results[phase].question;
-  console.log(data);
+  
   let respuesta1 = document.getElementById("respuesta-1");
   let respuesta2 = document.getElementById("respuesta-2");
   let respuesta3 = document.getElementById("respuesta-3");
   let respuesta4 = document.getElementById("respuesta-4");
+
   respuesta1.innerHTML = data.results[phase].incorrect_answers[0];
   respuesta2.innerHTML = data.results[phase].correct_answer;
   respuesta3.innerHTML = data.results[phase].incorrect_answers[1];
   respuesta4.innerHTML = data.results[phase].incorrect_answers[2];
+
+  respuesta1.addEventListener("click", function() {
+      // Chequear que sea verdadera
+      if (respuesta1.innerHTML === data.results[phase].correct_answer) {
+          console.log("Respuesta Correcta");
+      } else {
+          console.log("Respuesta incorrecta");
+      }
+
+      //Sumar puntos si es verdadera
+
+      //cambiar de phase si fase es menor de <= 9
+  })
+  respuesta2.addEventListener("click", function() {
+
+
+    // Chequear que sea verdadera
+    if (respuesta2.innerHTML === data.results[phase].correct_answer) {
+        console.log("Respuesta Correcta");
+    } else {
+        console.log("Respuesta incorrecta");
+    }
+
+    //Sumar puntos si es verdadera
+
+    //cambiar de phase si fase es menor de <= 9
+})
+respuesta3.addEventListener("click", function() {
+    // Chequear que sea verdadera
+    if (respuesta3.innerHTML === data.results[phase].correct_answer) {
+        console.log("Respuesta Correcta");
+    } else {
+        console.log("Respuesta incorrecta");
+    }
+
+    //Sumar puntos si es verdadera
+
+    //cambiar de phase si fase es menor de <= 9
+})
+respuesta4.addEventListener("click", function() {
+    // Chequear que sea verdadera
+    if (respuesta4.innerHTML === data.results[phase].correct_answer) {
+        console.log("Respuesta Correcta");
+    } else {
+        console.log("Respuesta incorrecta");
+    }
+
+    //Sumar puntos si es verdadera
+
+    //cambiar de phase si fase es menor de <= 9
+})
+
+
 }
