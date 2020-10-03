8466ff8d9f7edb8780b735f0e5796f5eafca66f6
Javiuty <javiiescuadra@gmail.com>
2020-10-03 23:19:53 +0200

    adding feature view all and reduce

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let viewAll = document.querySelector(".view-all");
+let categoriesBody = document.querySelector(".categories__body");
+
+let hide1 = document.querySelector(".hide1");
+let hide2 = document.querySelector(".hide2");
+let hide3 = document.querySelector(".hide3");
+
+viewAll.addEventListener("click", expand);
+
+function expand(event) {
+  if (event.target.textContent === "View All") {
+    viewAll.textContent = "Reduce";
+    categoriesBody.classList.remove("categories__body");
+    categoriesBody.classList.add("ampliar");
+    hide1.classList.remove("hide1");
+    hide2.classList.remove("hide2");
+    hide3.classList.remove("hide3");
+  } else {
+    viewAll.textContent = "View All";
+    categoriesBody.classList.remove("ampliar");
+    categoriesBody.classList.add("categories__body");
+    hide1.classList.add("hide1");
+    hide2.classList.add("hide2");
+    hide3.classList.add("hide3");
+  }
+}
